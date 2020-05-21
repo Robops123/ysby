473c05b05a86dc69c5278dc49b160c366c04c5d7
robops123 <806090884@qq.com>
2020-05-21 17:27:36 +0800

    1

--- pro.android/HBuilder-Hello/app/src/main/assets/apps/__UNI__BD88342/www/static/js/request.js
+++ pro.android/HBuilder-Hello/app/src/main/assets/apps/__UNI__BD88342/www/static/js/request.js
@@ -1,9 +1,10 @@
 
 // const baseURL = 'http://192.168.1.157/yishuban2/app/index.php?i=2&c=entry&m=ewei_shopv2&do=mobile';   //测试
-const baseURL = 'http://yuying.qinshaozhuanshu.cn/web/merchant.php?i=2&c=site&a=entry&m=ewei_shopv2&do=web'; // http://admin.chinaduancai.com/upload/  //正式
+const baseURL = 'http://192.168.1.157/yishuban2/web/merchant.php?i=2c=site&a=entry&m=ewei_shopv2&do=web'; // http://admin.chinaduancai.com/upload/  //正式
 // 参数： url:请求地址  param：请求参数  way：请求方式 res：回调函数http://192.168.1.157/yishuban2/web/merchant.php?i=2c=site&a=entry&m=ewei_shopv2&do=web
+
  /**
-  *  @name 请求
+  *  @name 请求http://yuying.qinshaozhuanshu.cn/web/merchant.php?i=2&c=site&a=entry&m=ewei_shopv2&do=web
   */
  async function apiPost(url,params){
  	// if(!params)console.warn("没有传入参数params");
@@ -13,7 +14,7 @@ const baseURL = 'http://yuying.qinshaozhuanshu.cn/web/merchant.php?i=2&c=site&a=
  			method :"POST",
  			header:{
  				// 'Token': token,
- 				'Accept': 'application/json',
+ 				// 'Accept': 'application/json',
  				"content-type":"application/x-www-form-urlencoded"
  			},
  			data:params,
@@ -52,7 +53,7 @@ const baseURL = 'http://yuying.qinshaozhuanshu.cn/web/merchant.php?i=2&c=site&a=
  			url:baseURL+url,
  			method :"GET",
  			header:{
- 				'Accept': 'application/json',
+ 				// 'Accept': 'application/json',
  				"content-type":"application/x-www-form-urlencoded"
  			},
  			timeout:6000,
